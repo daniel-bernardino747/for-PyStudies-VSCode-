fef4d74b2a5b6786aa321a2735c630c55e897009
Daniel <dn.danielbernardino@gmail.com>
2022-06-15 11:23:28 -0300

    return values

--- PySimpleGUI/windows4.py
+++ PySimpleGUI/windows4.py
@@ -8,12 +8,12 @@ layout = [
     [sg.Button('Show'), sg.Button('Exit')],
 ]
 
-window =  sg.Window('Janela', layout)
+window =  sg.Window('Janela', layout, enable_close_attempted_event=True)
 
 while True:
     event, values = window.read()
     print(event, values)
-    if event == sg.WIN_CLOSED or event == 'Exit':
+    if (event == sg.WINDOW_CLOSE_ATTEMPTED_EVENT or event == 'Exit') and sg.popup_yes_no('Do you really want to exit?') == 'Yes':
         break
     if event == 'Show':
         window['-OUTPUT-'].update(values['-IN-'])
